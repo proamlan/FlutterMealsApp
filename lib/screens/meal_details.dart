8c5d45c520b7019ce6e38ae2c95ae4b81cbe69b4
Amlan <amlana.dwivedy@someshwara.com>
2023-07-13 18:13:51 +0530

    Details screen UI completed

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/meals.dart';
 import 'package:transparent_image/transparent_image.dart';
 
-
 class MealDetailsScreen extends StatelessWidget {
-
   final Meal meal;
 
   const MealDetailsScreen({super.key, required this.meal});
-
 
   @override
   Widget build(BuildContext context) {
@@ -17,17 +14,60 @@ class MealDetailsScreen extends StatelessWidget {
         title: Text(meal.title),
       ),
       body: Container(
-        child: Column(
+        child: SingleChildScrollView(
+          child: Column(
           children: [
-            FadeInImage(placeholder: MemoryImage(kTransparentImage),
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
               fit: BoxFit.cover,
               width: double.infinity,
-            )
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Ingredients",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            for (final ingrediant in meal.ingredients)
+              Text(
+                ingrediant,
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Steps",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            for (final step in meal.steps)
+              Text(step,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ))
           ],
         ),
       ),
+      )
     );
   }
-
 }
